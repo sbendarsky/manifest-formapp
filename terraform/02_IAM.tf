d6cf1fec5b71f26f69bdcebfe03fc25e6c72bb6e
Stav Bendarsky <sbendarsky@gmail.com>
2024-01-08 19:46:32 +0200

    Add IAM and EKS configurations

---
+++ terraform/02_IAM.tf
@@ -0,0 +1,24 @@
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks_cluster"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+
+}
+
+resource "aws_iam_role_policy_attachment" "formapp_amazon_eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
